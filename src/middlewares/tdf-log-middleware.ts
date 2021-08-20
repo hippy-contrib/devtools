1ce7335bbbb808938dbf6ab4c37902a525dc8e9c
minushuang <minushuang@tencent.com>
2021-08-20 17:18:34 +0800

    feat: tdf-log-middleware 日志信息修改 (merge request !2)
    Squash merge branch 'fix/tdflog-timestamp' into 'master'
    
    * feat: tdf-log-middleware 日志信息修改

--- src/middlewares/tdf-log-middleware.ts
+++ src/middlewares/tdf-log-middleware.ts
@@ -6,13 +6,14 @@ export const onGetTDFLog: MiddleWare = async (ctx) => {
   const { params } = eventRes;
   try {
     params.log.forEach((log) => {
-      const logPrefix = `[${log.timestamp}][${log.level}][${log.source}]`;
+      const timestamp = `${new Date(Math.floor(log.timestamp / 1000000)).toLocaleString()}(${log.timestamp})`; // 换算为毫秒， 1毫秒 = 1000000纳秒
+      const logPrefix = `[${timestamp}][${log.level}][${log.source}]`;
       const consoleMessage = {
         source: 'other',
         level: 'info',
         text: log.module ? `${logPrefix}[${log.module}]${log.message}` : `${logPrefix}${log.message}`,
         lineNumber: log.line_number,
-        timestamp: new Date().getTime(),
+        timestamp,
         url: log.file_name,
       };
       ctx.sendToDevtools({
