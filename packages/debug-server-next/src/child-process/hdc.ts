8d30a6fc2bec51323a5325740ac56621c359197d
zealotchen <zealotchen@tencent.com>
2024-07-09 18:02:36 +0800

    feat(ohos): init

---
+++ packages/debug-server-next/src/child-process/hdc.ts
@@ -0,0 +1,57 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * Hippy available.
+ *
+ * Copyright (C) 2017-2019 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import path from 'path';
+import os from 'os';
+import fs from 'fs';
+import { exec } from '@debug-server-next/utils/process';
+import { WinstonColor, OSType } from '@debug-server-next/@types/enum';
+import { Logger } from '@debug-server-next/utils/log';
+import { config } from '@debug-server-next/config';
+
+let hmrPort;
+const log = new Logger('hdc', WinstonColor.Magenta);
+
+export const startHdcProxy = async () => {
+  if (!hmrPort) {
+    try {
+      const data = fs.readFileSync(config.hmrPortPath);
+      hmrPort = data.toString();
+    } catch (e) {
+      log.verbose('hmrPort file does not exist.');
+    }
+  }
+  const { port } = global.debugAppArgv || {};
+  const hdcRelatePath = {
+    [OSType.Darwin]: '../build/mac/hdc',
+    [OSType.Windows]: '../build/win/hdc.exe',
+  }[os.type()];
+  const hdcPath = path.join(__dirname, hdcRelatePath);
+  try {
+    if (port) await exec(hdcPath, ['rport', `tcp:${port}`, `tcp:${port}`]);
+    if (hmrPort) await exec(hdcPath, ['rport', `tcp:${hmrPort}`, `tcp:${hmrPort}`]);
+  } catch (e) {
+    log.warn('Port reverse failed, For iOS app just ignore this message.');
+    log.warn(`Otherwise please check 'hdc devices' command working correctly`);
+    if (port) log.warn(`type 'hdc reverse tcp:${port} tcp:${port}' retry!`);
+    if (hmrPort) log.warn(`type 'hdc reverse tcp:${hmrPort} tcp:${hmrPort}' retry!`);
+    log.warn('start hdc reverse error: %s', (e as Error)?.stack);
+  }
+};
